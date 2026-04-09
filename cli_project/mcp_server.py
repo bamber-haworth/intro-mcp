d6b341759f7b17ea8ea5bbc71727718d3b7cb200
Bamber Haworth <bamber@audiostack.ai>
2026-04-09 17:53:02 +0100

    add resources

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -40,7 +40,26 @@ def edit_document(
     docs[doc_id] = docs[doc_id].replace(old_string, new_content)    
 
 # TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
+
+mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Error: Document with id '{doc_id}' not found.")
+    return docs[doc_id]
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
